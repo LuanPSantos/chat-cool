a10e0eb302ca252b5be4eb38b845124d78f4968f
Luan <santos.pereira.luan@gmail.com>
2022-03-10 20:27:24 -0300

    Initial commit

---
+++ src/main/kotlin/codeplays/trainee/chatcool/CoolChatApplication.kt
@@ -0,0 +1,11 @@
+package codeplays.trainee.chatcool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoolChatApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoolChatApplication>(*args)
+}
